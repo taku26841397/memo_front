cac87e8b74658227881578102d9768fbe7dd4641
username <user@example>
2026-09-01 18:24:03 +0900

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import cloudflare from "@cloudflare/vite-plugin";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), cloudflare()],
+});
+
